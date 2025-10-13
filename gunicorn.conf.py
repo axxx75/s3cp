a6c3cda535271ff5e9796a147c0323703afc5f92
Axxx <axxxa54@gmail.com>
2025-10-13 21:15:03 +0200

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+import multiprocessing
+
+# Numero di worker (uno per core)
+workers = multiprocessing.cpu_count()
+
+# Host e porta
+bind = "0.0.0.0:8080"
+
+# Log file persistenti
+errorlog = "/var/log/s3cputo/error.log"
+accesslog = "/var/log/s3cputo/access.log"
+
+# Livello di log
+loglevel = "debug"
+
+# Formato log accesso
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
